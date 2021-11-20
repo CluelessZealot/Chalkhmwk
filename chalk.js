0e4d74380c2b6bc76f09de1739f58b6fcf72282f
Shane Parenteau <sp0555121@otc.edu>
2021-11-20 07:01:47 -0600

    added read me

---
+++ chalk.js
@@ -0,0 +1,25 @@
+const chalk = require('chalk');
+const chalkAnimation = require('chalk-animation');
+const log = console.log;
+void Animate();{
+    setTimeout(() => {
+
+        const ending = chalkAnimation.rainbow('lights');
+    
+    }, 14000);
+};
+
+
+console.log(chalk.blue.bgRed.bold("weave through snowflakes"), ("\n"),  
+chalk.rgb(103, 45, 167)("teasing"),  chalk.rgb(0, 45, 267)("and"),  chalk.white.underline("twirling about"), ("\n"),
+    chalk.blue("distant")
+);
+chalkAnimation.glitch("stars dancing");
+chalkAnimation.neon("a brilliant display");
+
+console.log(("\n"),
+chalk.green("wearing only"), chalk.green.underline("hand-me-downs"), ("\n"),
+chalk.grey("the moon"), chalk.hex('green').bold("steals the show"), ("\n"),
+
+chalk.red("~FJ Thomas"), ("\n"),
+chalk.yellow("01/01/2020"));
